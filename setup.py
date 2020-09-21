6bf3b0930909c78e3b59288c378c9985bc3cdb26
Daniel Clarke <u8sand@gmail.com>
2020-09-21 15:58:00 -0400

    Added missing requirements.txt

---
+++ setup.py
@@ -0,0 +1,21 @@
+import json
+from setuptools import setup, find_packages
+
+packageJson = json.load(open('package.json', 'r'))
+
+setup(
+  name='react_gsea',
+  version=packageJson['version'],
+  description=packageJson['description'],
+  url=packageJson['homepage'],
+  author=packageJson['author'],
+  license=packageJson['license'],
+  long_description=open('README.md', 'r').read(),
+  packages=find_packages(),
+  include_package_data=True,
+  package_data={'react_gsea': [
+    'package-info.json',
+    'react_gsea.min.js',
+  ]},
+  requires=['dash'],
+)
