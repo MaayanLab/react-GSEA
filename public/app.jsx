8fd4dca4e51a1f2ad0d707c2c45bc63651e44d76
Daniel Clarke <u8sand@gmail.com>
2020-09-21 10:57:04 -0400

    Added code base

---
+++ public/app.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import ReactGSEA, { dataFromResults } from '../src/index'
+
+function randint(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function shuffle(a) {
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
+}
+
+function random_choice(L) {
+  return L[randint(0, L.length-1)]
+}
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render = () => {
+    const entities = 'abcdefghijklmnopqrstuvwxyz'.split('')
+    let shuffled_entities = entities
+    shuffle(shuffled_entities)
+    const up = shuffled_entities.slice(0, 10)
+    shuffle(shuffled_entities)
+    const down = shuffled_entities.slice(0, 10)
+    shuffle(shuffled_entities)
+    const ranks = shuffled_entities.reduce((agg, ent, ind) => ({...agg, [ent]: ind}), {})
+    console.log({ up, down, ranks })
+    return (
+      <div>
+        <ReactGSEA
+          data={dataFromResults({
+            input: {
+              up,
+              down,
+            },
+            output: {
+              entities,
+              ranks: entities.map(ent => ranks[ent]),
+            },
+          })}
+        />
+      </div>
+    )
+  }
+}
