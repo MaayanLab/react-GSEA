43ea1516376a78d5a9ada10fd8790c6a8e1ea5fa
Daniel Clarke <u8sand@gmail.com>
2020-09-21 14:03:45 -0400

    Add example dash app

---
+++ example/example.py
@@ -0,0 +1,41 @@
+import sys; sys.path.insert(0, '..') # not necessary when pip installed
+import os
+import json
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+from react_gsea import ReactGSEA, dataFromResult
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div(className='row', children=[
+  html.Div(
+    className='col-sm-6',
+    children=[
+      ReactGSEA(
+        data=dataFromResult({
+          'up': [
+            'STAT3',
+          ],
+          'down': [
+            'CD58',
+          ],
+        }, {
+          'entities': [
+            'STAT3', 'STAT2', 'CD58', 'STAT1'
+          ],
+          'ranks': [
+            1, 0, 2, 3,
+          ],
+        }),
+      ),
+    ],
+  ),
+])
+
+app.run_server(
+  host=os.environ.get('HOST', '0.0.0.0'),
+  port=json.loads(os.environ.get('PORT', '8050')),
+  debug=json.loads(os.environ.get('DEBUG', 'true')),
+)
